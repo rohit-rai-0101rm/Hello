89ba0ee8034adbf54f75753e1668018dc64958d2
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2024-03-21 04:07:41 +0530

    remote image rendered

--- Hello/ContentView.swift
+++ Hello/ContentView.swift
@@ -8,15 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let imageUrl :String="https://media.geeksforgeeks.org/wp-content/uploads/20230617194030/Getting-Started-with-iOS-Development.webp"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!").font(.system(size: 80))
-                .fontWeight(.black).foregroundStyle(LinearGradient(colors:  [.pink,.purple,.blue], startPoint: .topLeading, endPoint: .bottomTrailing))
-        }
-        .padding()
+       
+      //Mark -1.Basic
+        AsyncImage(url:URL(string: imageUrl))
     }
 }
 
