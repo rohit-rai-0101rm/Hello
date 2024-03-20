a5a157878c2646c2137b2111812bf277c04caf0b
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2024-03-20 08:28:14 +0530

    Initial Commit

---
+++ Hello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello
+//
+//  Created by Rohit Rai on 20/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
